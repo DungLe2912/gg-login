db130fcdad43de72c28ce65fb2b6014a541d6068
DungLe2912 <edward.le.goldenowl@gmail.com>
2023-08-03 22:21:40 +0700

    test open default browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import "./App.css";
 import LoginButton from "./LoginButton";
 function App() {
+  useEffect(() => {
+    window.open("https://www.w3schools.com", "_blank");
+  }, []);
   return (
     <div className="App">
       <LoginButton />
