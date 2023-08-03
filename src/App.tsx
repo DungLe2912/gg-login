4e7e7a56559c458203408d0e46e84caa689b0fd8
DungLe2912 <edward.le.goldenowl@gmail.com>
2023-08-03 22:30:50 +0700

    chore: open in new window

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 import LoginButton from "./LoginButton";
 function App() {
   useEffect(() => {
-    window.open("https://www.w3schools.com", "_blank");
+    // window.open("https://www.w3schools.com", "_blank");
+    window.open("http://stackoverflow.com", "Stack_Overflow", "location=0");
   }, []);
   return (
     <div className="App">
