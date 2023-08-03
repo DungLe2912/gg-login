9e34ae64967f604633405912fe888c56f7d58921
Edward Le <edward.le.goldenowl@gmail.com>
2023-08-03 09:59:07 +0700

    chore: fix issue not render button

--- src/LoginButton.tsx
+++ src/LoginButton.tsx
@@ -1,26 +1,79 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-function LoginButton() {
-  const handleCallbackResponse = (response: any) => {
-    console.log("id token: ", response.credential);
-  };
+// function LoginButton() {
+//   const handleCallbackResponse = (response: any) => {
+//     console.log("id token: ", response.credential);
+//   };
+
+//   useEffect(() => {
+//     if (typeof window.google !== "undefined") {
+//       window.google.accounts.id.initialize({
+//         client_id:
+//           "300192009869-1cms7vtbqgem3ognlgg9gi428t2fljl7.apps.googleusercontent.com",
+//         callback: handleCallbackResponse,
+//       });
+
+//       window.google.accounts.id.renderButton(
+//         document.getElementById("signInDiv"),
+//         { theme: "outline", size: "large" }
+//       );
+//     }
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
+//   return <div id="signInDiv" style={{ width: "fit-content" }}></div>;
+// }
+
+// export default LoginButton;
+
+//External imports
+import { useEffect, useRef } from "react";
+
+const loadScript = (src: string) =>
+  new Promise<void>((resolve, reject) => {
+    if (document.querySelector(`script[src="${src}"]`)) return resolve();
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => resolve();
+    script.onerror = (err) => reject(err);
+    document.body.appendChild(script);
+  });
+
+const LoginButton = () => {
+  const googleButton = useRef(null);
 
   useEffect(() => {
-    if (typeof window.google !== "undefined") {
-      window.google.accounts.id.initialize({
-        client_id:
-          "300192009869-1cms7vtbqgem3ognlgg9gi428t2fljl7.apps.googleusercontent.com",
-        callback: handleCallbackResponse,
-      });
+    const src = "https://accounts.google.com/gsi/client";
+    const id =
+      "300192009869-1cms7vtbqgem3ognlgg9gi428t2fljl7.apps.googleusercontent.com";
 
-      window.google.accounts.id.renderButton(
-        document.getElementById("signInDiv"),
-        { theme: "outline", size: "large" }
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    loadScript(src)
+      .then(() => {
+        console.log(window.google);
+        window.google.accounts.id.initialize({
+          client_id: id,
+          callback: handleCredentialResponse,
+        });
+        window.google.accounts.id.renderButton(googleButton.current, {
+          theme: "outline",
+          size: "large",
+        });
+      })
+      .catch(console.error);
+
+    return () => {
+      const scriptTag = document.querySelector(`script[src="${src}"]`);
+      // if (scriptTag) document.body.removeChild(scriptTag);
+      if (scriptTag && scriptTag.parentNode === document.body) {
+        document.body.removeChild(scriptTag);
+      }
+    };
   }, []);
-  return <div id="signInDiv" style={{ width: "fit-content" }}></div>;
-}
+
+  function handleCredentialResponse(response: any) {
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+
+  return <div ref={googleButton} style={{ width: "fit-content" }}></div>;
+};
 
 export default LoginButton;
